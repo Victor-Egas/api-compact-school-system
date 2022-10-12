9d0d9d528f92c0ecfcd422a1582b9258cee8b2a8
Victor-Egas <victor.egas.fieet@gmail.com>
2022-10-11 23:22:25 -0500

    shore : Init project

---
+++ src/main/java/com/compact/school/school/ApiCompactSchoolSystemApplication.java
@@ -0,0 +1,13 @@
+package com.compact.school.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCompactSchoolSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCompactSchoolSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compact/school/school/ApiCompactSchoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compact.school.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCompactSchoolSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
